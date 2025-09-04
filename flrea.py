8f14168492513300f669efe8008bcb86585696f3
pardhu bhagyam <pardhu20040501778@gmail.com>
2025-09-04 12:30:02 +0530

    database

--- flrea.py
+++ flrea.py
@@ -56,5 +56,8 @@ def update(id):
 
 if __name__ == '__main__':
     import os
+    if not os.path.exists('learn.db'):
+        with app.app_context():
+            db.create_all()
     port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
     app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
